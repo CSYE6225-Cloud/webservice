a25be40a73069ad49e5cf1cc942a6cb031e08926
CYan <hu_yancheng@outlook.com>
2022-05-14 15:42:01 -0400

    log4j2 config

--- src/main/java/com/chengyan/webapp/WebappApplication.java
+++ src/main/java/com/chengyan/webapp/WebappApplication.java
@@ -1,6 +1,8 @@
 package com.chengyan.webapp;
 
 import com.chengyan.webapp.ServiceController.StatsD;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,12 +16,16 @@ public class WebappApplication {
 	@Autowired
 	private StatsD statsd;
 
+	private static final Logger logger = LogManager.getLogger(WebappApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(WebappApplication.class, args);
 	}
 
 	@GetMapping("/healthz")
 	public void healthz() {
+		logger.debug("Debugging log");
+		logger.info("Info log");
 		statsd.getStatsd().incrementCounter("/healthz.http.get");
 		return;
 	}
